1567b12f38ba2815d5636c33c00758084b551266
AndyB-WHG <andrew.bond2@gmail.com>
2021-04-28 18:23:50 +0000

    fix: added the operands to be calculated.

--- script.js
+++ script.js
@@ -10,8 +10,36 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked Submit!");                
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
+
+function runGame(gameType) {
+    //Generate two random numbers between 1 and 25
+    //Math.floor rounds down the whole number 
+    //Math.random generates random numbers
+    // +1 adds 1 to the final value to start at 1 instead of 0
+
+    let num1 = Math.floor(Math.random() * 25)+1;
+    console.log(num1)
+    let num2 = Math.floor(Math.random() * 25)+1;
+    console.log(num2)
+
+    if(gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert (`Unknown game type ${gameType}`);
+        throw `Unknown game type ${gameType}, aborting!`;
+    }
+
+}
+
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
+}
